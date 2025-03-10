bd27d1e3f43dc881555e9150ce228036309c3659
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2025-03-09 22:37:40 -0300

    feature: Adding email validator

---
+++ Vilas.Utils/Email.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Vilas.Utils.Text
+{
+    public  static class Email
+    {
+        public static bool Validate(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
+            string emailPattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            return Regex.IsMatch(email, emailPattern, RegexOptions.IgnoreCase);
+        }
+    }
+}
